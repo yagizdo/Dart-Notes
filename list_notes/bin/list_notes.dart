0701c67475d47fe3704b1af9f0da72a423a4353f
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2021-10-10 21:49:16 +0300

    Updated list notes

--- list_notes/bin/list_notes.dart
+++ list_notes/bin/list_notes.dart
@@ -2,11 +2,11 @@ import 'dart:io';
 
 void main() {
   var names = <String>[];
-  // TR - Listeye eleman ekleme
-  // ENG - Adding an element to the list
-  names.add('Oğuzhan İnce'); // 0. Eleman
-  names.add('Selim Yalınkılıç'); // 1. Eleman
-  names.add('Yılmaz Yağız Dokumacı'); // 2. Eleman
+  // TR - Listeye eleman ekleme.
+  // ENG - Adding an element to the list.
+  names.add('Oğuzhan İnce'); // 0. Eleman.
+  names.add('Selim Yalınkılıç'); // 1. Eleman.
+  names.add('Yılmaz Yağız Dokumacı'); // 2. Eleman.
 
   print(names);
 
@@ -58,13 +58,13 @@ void main() {
   var reversedList = names.reversed;
   print(reversedList);
 
-  // TR - Listeyi alfabetik olarak sıralatma
-  // ENG - Sort the list alphabetically
+  // TR - Listeyi alfabetik olarak sıralatma.
+  // ENG - Sort the list alphabetically.
   names.sort();
   print(names);
 
-  // TR - Listeden bir eleman silmek
-  // ENG - Deleting an element from the list
+  // TR - Listeden bir eleman silmek.
+  // ENG - Deleting an element from the list.
   names.removeAt(2);
   print(names);
 
@@ -73,8 +73,32 @@ void main() {
   names.remove("Oğuzhan İnce");
   print(names);
 
-  // TR - Listeyi direk temizlemek/içeriğini silmek
-  // ENG - Clearing/deleting the list directly
+  // TR - Listeyi direk temizlemek/içeriğini silmek.
+  // ENG - Clearing/deleting the list directly.
   names.clear();
   print(names);
+
+  /*
+  TR : Listelerin döngüler ile kullanılması.
+  ENG : Using lists with loops.
+  */
+
+  var fruits = <String>[];
+
+  fruits.add("Apple");
+  fruits.add("Banana");
+  fruits.add("Cherry");
+  fruits.add("Kiwi");
+  fruits.add("Strawberry");
+
+  // TR : Listede ki her elemanı tek tek f değişkenine aktarıyoruz. Ve sırayla aktardığımız değerleri yazdırıyoruz.
+  // ENG : We transfer each element in the list to the variable f one by one. And we print the values ​​we transferred in order.
+  for (var f in fruits) {
+    print("Fruit : $f");
+  }
+  // TR : Eğer içeriğin yanında index numarasına da erişmek istiyorsak, Bu şekilde bir for döngüsünü kullanarak hem liste içerisinde ki değerlere hemde index numaralarına erişebiliyoruz. Döngü sınırını da direk liste uzunluğu olarak verdik böylece listede ki elemanlar artıp azaldıkça for döngüsü de kendisini buna göre çalıştıracak.
+  // ENG : If we want to access the index number as well as the content, we can access both the values ​​in the list and the index numbers by using a for loop in this way. We also gave the loop limit directly as the list length, so as the elements in the list increase or decrease, the for loop will run itself accordingly.
+  for (var i = 0; i < (fruits.length); i++) {
+    print("Fruits in index $i : ${fruits[i]}");
+  }
 }
