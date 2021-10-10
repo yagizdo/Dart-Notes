bbec13e851537c2be139d6a629188331f0d8431d
Yılmaz Yağız Dokumacı <dokumaciyagiz@gmail.com>
2021-10-10 22:20:50 +0300

    Updated list notes

--- list_notes/bin/list_notes.dart
+++ list_notes/bin/list_notes.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'Students.dart';
+
 void main() {
   var names = <String>[];
   // TR - Listeye eleman ekleme.
@@ -48,10 +50,10 @@ void main() {
   // TR - Listenin içeriğinde bir eleman arıyoruz ve o eleman listede var ise true yok ise false döndürüyor.
   // ENG - We are looking for an element in the content of the list and it returns true if that element exists in the list, false otherwise.
   // Example 1 :
-  print(names.contains("Oğuzhan İnce"));
+  print(names.contains('Oğuzhan İnce'));
 
   // Example 2 :
-  print(names.contains("Kiraz"));
+  print(names.contains('Kiraz'));
 
   // TR - Listeyi ters çevirmek.
   // ENG - Reverse the list.
@@ -70,7 +72,7 @@ void main() {
 
   // TR - Listeden direk istediğimiz bir değeri girmek. Mesela bu örnekte liste içerisinde bu yazdığımız değerden birden fazla varsa hepsini siler. Yani 5 tane Oğuzhan İnce varsa hepsini siler. Üstte ki durumda sadece o indexi siler.
   // ENG - Entering a value we want directly from the list. For example, in this example, if there is more than one value in the list, it deletes all of them. In other words, if there are 5 Oğuzhan İnce, he will delete them all. In the above case, it only deletes that index.
-  names.remove("Oğuzhan İnce");
+  names.remove('Oğuzhan İnce');
   print(names);
 
   // TR - Listeyi direk temizlemek/içeriğini silmek.
@@ -85,20 +87,42 @@ void main() {
 
   var fruits = <String>[];
 
-  fruits.add("Apple");
-  fruits.add("Banana");
-  fruits.add("Cherry");
-  fruits.add("Kiwi");
-  fruits.add("Strawberry");
+  fruits.add('Apple');
+  fruits.add('Banana');
+  fruits.add('Cherry');
+  fruits.add('Kiwi');
+  fruits.add('Strawberry');
 
   // TR : Listede ki her elemanı tek tek f değişkenine aktarıyoruz. Ve sırayla aktardığımız değerleri yazdırıyoruz.
   // ENG : We transfer each element in the list to the variable f one by one. And we print the values ​​we transferred in order.
   for (var f in fruits) {
-    print("Fruit : $f");
+    print('Fruit : $f');
   }
   // TR : Eğer içeriğin yanında index numarasına da erişmek istiyorsak, Bu şekilde bir for döngüsünü kullanarak hem liste içerisinde ki değerlere hemde index numaralarına erişebiliyoruz. Döngü sınırını da direk liste uzunluğu olarak verdik böylece listede ki elemanlar artıp azaldıkça for döngüsü de kendisini buna göre çalıştıracak.
   // ENG : If we want to access the index number as well as the content, we can access both the values ​​in the list and the index numbers by using a for loop in this way. We also gave the loop limit directly as the list length, so as the elements in the list increase or decrease, the for loop will run itself accordingly.
   for (var i = 0; i < (fruits.length); i++) {
-    print("Fruits in index $i : ${fruits[i]}");
+    print('Fruits in index $i : ${fruits[i]}');
+  }
+
+  // TR : Listelerimizde objeleri de tutabiliriz. Mesela bu örnekte Students classından oluşturduğumuz nesneleri students listesinde tutuyoruz.
+  // ENG : We can also keep objects in our lists. For example, in this example, we keep the objects we created from the Students class in the students list.
+
+  var student1 = Students('Yılmaz', '9A', 543);
+  var student2 = Students('Selim', '10C', 653);
+  var student3 = Students('Oğuzhan', '8B', 126);
+  var student4 = Students('Ahmet', '11D', 873);
+
+  var students = <Students>[];
+
+  students.add(student1);
+  students.add(student2);
+  students.add(student3);
+  students.add(student4);
+
+  for (var s in students) {
+    print("*************************");
+    print("Student No : ${s.studentNo}");
+    print("Student Name : ${s.name}");
+    print("Student Class : ${s.classInfo}");
   }
 }
